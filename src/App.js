2d015374a508af868590dc72ceb5a5f31a7963d1
Alexander <1alexvash@gmail.com>
2020-09-19 17:38:58 +0300

    default HTML markup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
 
+import "./scss/main.css";
+
 const ramdomWords = ["apple", "orange", "pear", "plumb", "kiwi"];
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="App">
+      <button className="listen"></button>
+      <div className="tip">Click to listen the word again</div>
+      <div className="boxes">
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+      </div>
+      <div className="input"></div>
+    </div>
+  );
 };
 
 export default App;
