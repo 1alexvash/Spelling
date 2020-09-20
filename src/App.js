3ab86588a39a07492084aba12900e5354f12a394
Alexander <1alexvash@gmail.com>
2020-09-20 18:40:40 +0300

    Added typing functionality

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ const ramdomWords = ["apple", "orange", "pear", "plumb", "kiwi"];
 
 const App = () => {
   const [randomWord, setRandomWord] = useState(getRandomWord());
+  console.log("randomWord:", randomWord);
+  const [inputWord, setInputWord] = useState("");
 
   function getRandomWord() {
     const random = Math.floor(Math.random() * ramdomWords.length);
@@ -15,18 +17,53 @@ const App = () => {
     return randomWord;
   }
 
+  function typing(e) {
+    console.log(randomWord.length);
+    if (randomWord.length === 0) {
+      alert("Functionality for changing new word is not implemented yet");
+      return;
+    }
+
+    if (e.target.value === randomWord[0]) {
+      setInputWord(inputWord + e.target.value);
+      setRandomWord(randomWord.substring(1));
+      e.target.value = "";
+      alert("Correct");
+
+      if (randomWord.length === 1) {
+        alert("Good next. Let's proceed to the next round");
+      }
+    } else {
+      e.target.value = "";
+      alert("Not correct");
+    }
+    // console.log("randomWord:", randomWord);
+    // console.log("inputWord:", inputWord);
+    // console.log(letter);
+  }
+
   return (
     <div className="App">
       <img src={microImg} alt="" className="listen" />
       <div className="tip">Click to listen the word again</div>
       <div className="boxes">
+        {inputWord.split("").map((letter, index) => (
+          <div className="box active" key={index}>
+            {letter}
+          </div>
+        ))}
         {randomWord.split("").map((letter, index) => (
           <div className="box" key={index}>
             ?
           </div>
         ))}
       </div>
-      <input type="text" className="input" placeholder="Start typing" />
+      <input
+        type="text"
+        className="input"
+        placeholder="Start typing"
+        onChange={(e) => typing(e)}
+      />
     </div>
   );
 };
