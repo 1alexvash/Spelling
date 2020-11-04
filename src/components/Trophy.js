bb2f2ef664fc67b451a5ff6f6f955590b97f9899
Alexander <1alexvash@gmail.com>
2020-11-04 15:46:30 +0200

    Added system to show points of current level

--- src/components/Trophy.js
+++ src/components/Trophy.js
@@ -4,22 +4,30 @@ import trophyImg from "../images/trophy.png";
 
 const Trophy = ({ points }) => {
   const levels = [0, 10, 20, 50, 75, 100, 150, 200, 300, 500];
-  let level = 1;
+  let level = null;
+  let nextLevel = null;
 
   for (let i = 0; i < levels.length; i++) {
     if (points >= levels[i]) {
-      level = i + 1;
+      level = i;
+      nextLevel = level + 1;
     }
+  }
+
+  function getProgress() {
+    const pointsForLevelUp = levels[nextLevel] - levels[level];
+    const levelPoints = points - levels[level];
+    return (levelPoints / pointsForLevelUp) * 100 + "%";
   }
 
   return (
     <div className="trophy">
       <img src={trophyImg} alt="" />
       <div className="level" title="Current level:">
-        {level}
+        {level + 1}
       </div>
       <div className="bar">
-        <div className="progress" style={{ width: points + "%" }}></div>
+        <div className="progress" style={{ width: getProgress() }}></div>
       </div>
     </div>
   );
