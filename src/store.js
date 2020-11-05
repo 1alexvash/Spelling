ed2c2f356fe5dfe3a4fafce994f9c11691188d1d
Alexander <1alexvash@gmail.com>
2020-11-05 11:55:01 +0200

    Points are cached in the memory

--- src/store.js
+++ src/store.js
@@ -3,11 +3,12 @@ import { createStore, action } from "easy-peasy";
 const store = createStore({
   levels: [0, 10, 20, 50, 75, 100, 150, 200, 300, 500],
   difficulty: [2000, 1800, 1600, 1400, 1200, 1000, 800, 700, 500, 500],
-  points: 0,
+  points: localStorage.points ? JSON.parse(localStorage.points) : 0,
   level: null,
   nextLevel: null,
   setPoints: action((state, payload) => {
     state.points += payload;
+    localStorage.points = state.points;
 
     for (let i = 0; i < state.levels.length; i++) {
       if (state.points >= state.levels[i]) {
