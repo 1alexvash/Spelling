cfdfd60985da844dc87b036b534ebaf352e7c006
Alexander <1alexvash@gmail.com>
2020-11-22 19:20:49 +0200

    added active class on active language

--- src/components/LanguageSwitch.js
+++ src/components/LanguageSwitch.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 import britishFlagImg from "../images/british_flag.png";
 import americanFlagImg from "../images/american_flag.png";
 
 const LanguageSwitch = () => {
+  const [language, setLanguage] = useState("American");
+
   return (
     <div className="language-switch">
-      <img src={britishFlagImg} alt="" />
-      <img src={americanFlagImg} alt="" />
+      <img
+        src={britishFlagImg}
+        className={language === "American" ? "active" : "not-active"}
+        alt=""
+      />
+      <img
+        src={americanFlagImg}
+        className={language === "British" ? "active" : "not-active"}
+        alt=""
+      />
     </div>
   );
 };
