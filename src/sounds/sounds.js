6609b8f0991b6182dd4f328959e270b679549f25
Alexander <1alexvash@gmail.com>
2020-11-23 17:10:37 +0200

    added suffixes to sounds import

--- src/sounds/sounds.js
+++ src/sounds/sounds.js
@@ -1,61 +1,114 @@
-import A from "./A.mp3";
-import B from "./B.mp3";
-import C from "./C.mp3";
-import D from "./D.mp3";
-import E from "./E.mp3";
-import F from "./F.mp3";
-import G from "./G.mp3";
-import H from "./H.mp3";
-import I from "./I.mp3";
-import J from "./J.mp3";
-import K from "./K.mp3";
-import L from "./L.mp3";
-import M from "./M.mp3";
-import N from "./N.mp3";
-import O from "./O.mp3";
-import P from "./P.mp3";
-import Q from "./Q.mp3";
-import R from "./R.mp3";
-import S from "./S.mp3";
-import T from "./T.mp3";
-import U from "./U.mp3";
-import V from "./V.mp3";
-import W from "./W.mp3";
-import X from "./X.mp3";
-import Y from "./Y.mp3";
-import Z from "./Z.mp3";
+import A_american from "./american/A.mp3";
+import B_american from "./american/B.mp3";
+import C_american from "./american/C.mp3";
+import D_american from "./american/D.mp3";
+import E_american from "./american/E.mp3";
+import F_american from "./american/F.mp3";
+import G_american from "./american/G.mp3";
+import H_american from "./american/H.mp3";
+import I_american from "./american/I.mp3";
+import J_american from "./american/J.mp3";
+import K_american from "./american/K.mp3";
+import L_american from "./american/L.mp3";
+import M_american from "./american/M.mp3";
+import N_american from "./american/N.mp3";
+import O_american from "./american/O.mp3";
+import P_american from "./american/P.mp3";
+import Q_american from "./american/Q.mp3";
+import R_american from "./american/R.mp3";
+import S_american from "./american/S.mp3";
+import T_american from "./american/T.mp3";
+import U_american from "./american/U.mp3";
+import V_american from "./american/V.mp3";
+import W_american from "./american/W.mp3";
+import X_american from "./american/X.mp3";
+import Y_american from "./american/Y.mp3";
+import Z_american from "./american/Z.mp3";
+
+import A_british from "./british/A.mp3";
+import B_british from "./british/B.mp3";
+import C_british from "./british/C.mp3";
+import D_british from "./british/D.mp3";
+import E_british from "./british/E.mp3";
+import F_british from "./british/F.mp3";
+import G_british from "./british/G.mp3";
+import H_british from "./british/H.mp3";
+import I_british from "./british/I.mp3";
+import J_british from "./british/J.mp3";
+import K_british from "./british/K.mp3";
+import L_british from "./british/L.mp3";
+import M_british from "./british/M.mp3";
+import N_british from "./british/N.mp3";
+import O_british from "./british/O.mp3";
+import P_british from "./british/P.mp3";
+import Q_british from "./british/Q.mp3";
+import R_british from "./british/R.mp3";
+import S_british from "./british/S.mp3";
+import T_british from "./british/T.mp3";
+import U_british from "./british/U.mp3";
+import V_british from "./british/V.mp3";
+import W_british from "./british/W.mp3";
+import X_british from "./british/X.mp3";
+import Y_british from "./british/Y.mp3";
+import Z_british from "./british/Z.mp3";
 
 import good from "./good.mp3";
 import bad from "./bad.mp3";
 import new_level from "./new_level.mp3";
 
 export default {
-  A,
-  B,
-  C,
-  D,
-  E,
-  F,
-  G,
-  H,
-  I,
-  J,
-  K,
-  L,
-  M,
-  N,
-  O,
-  P,
-  Q,
-  R,
-  S,
-  T,
-  U,
-  V,
-  W,
-  X,
-  Y,
-  Z,
+  A_american,
+  B_american,
+  C_american,
+  D_american,
+  E_american,
+  F_american,
+  G_american,
+  H_american,
+  I_american,
+  J_american,
+  K_american,
+  L_american,
+  M_american,
+  N_american,
+  O_american,
+  P_american,
+  Q_american,
+  R_american,
+  S_american,
+  T_american,
+  U_american,
+  V_american,
+  W_american,
+  X_american,
+  Y_american,
+  Z_american,
+  A_british,
+  B_british,
+  C_british,
+  D_british,
+  E_british,
+  F_british,
+  G_british,
+  H_british,
+  I_british,
+  J_british,
+  K_british,
+  L_british,
+  M_british,
+  N_british,
+  O_british,
+  P_british,
+  Q_british,
+  R_british,
+  S_british,
+  T_british,
+  U_british,
+  V_british,
+  W_british,
+  X_british,
+  Y_british,
+  Z_british,
   good,
   bad,
   new_level,
